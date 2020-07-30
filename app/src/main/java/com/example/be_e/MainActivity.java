ca1a608094264d6318a862204d41dea6eb9c4fd9
HEEJU <jhjoo0303@naver.com>
2020-07-31 00:01:45 +0900

    1 Tmap API 2 Tmap 띄우기 3 마커 띄우기

--- app/src/main/java/com/example/be_e/MainActivity.java
+++ app/src/main/java/com/example/be_e/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.be_e;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
+        TMapView tMapView = new TMapView(this);
+
+        TMapMarkerItem markerItem1 = new TMapMarkerItem();
+
+        TMapPoint tMapPoint1 = new TMapPoint(37.570841, 126.985302); // SKT타워
+
+        // 마커 아이콘
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.lomaker);
+
+        markerItem1.setIcon(bitmap); // 마커 아이콘 지정
+        markerItem1.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
+        markerItem1.setTMapPoint( tMapPoint1 ); // 마커의 좌표 지정
+        markerItem1.setName("SKT타워"); // 마커의 타이틀 지정
+        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
+        tMapView.setCenterPoint( 126.985302, 37.570841 );
+
+        tMapView.setSKTMapApiKey( "l7xxaf5e2f6967094cf39c3ff1fdda6f3af6");
+        linearLayoutTmap.addView( tMapView );
     }
+
+
+
+
 }
